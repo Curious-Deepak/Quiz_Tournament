5d25f5b5719ee6ae93648408c7cf7a76d9551827
Deepak <itsdeepakbharti@gmail.com>
2026-05-08 04:02:33 +0530

    Login-error handling added

--- docs/js/login.js
+++ docs/js/login.js
@@ -14,14 +14,28 @@ document.getElementById("loginForm").addEventListener("submit", async function (
         body: JSON.stringify(data)
     });
 
-    const result = await res.json();
+    let result;
 
-    if (result.token) {
-        localStorage.setItem("token", result.token);
-        window.location.href = "index.html";
-    } else {
-        alert(result.message);
+    try {
+        result = await res.json();
+    } catch (e) {
+        showLoginError(" Server error ! ");
+        return;
     }
+
+    if (res.ok && result.token) {
+        localStorage.setItem("token", result.token);
+        window.location.replace("index.html");
+    } else {
+        showLoginError(result.message || " Invalid email or password ");
+
+        document.getElementById("loginEmail").value = "";
+        document.getElementById("loginPassword").value = "";
+        
+        document.getElementById("loginEmail").style.border = "1px solid red";
+        document.getElementById("loginPassword").style.border = "1px solid red";
+    }
+
 });
 
 
@@ -42,18 +56,40 @@ document.getElementById("signupForm").addEventListener("submit", async function 
         body: JSON.stringify(data)
     });
 
-    const result = await res.text();
-    alert(result);
+    let result;
+
+    try {
+        result = await res.json();
+    } catch (e) {
+        showLoginError("Server error");
+        return;
+    }
+
+    if (res.ok) {
+        window.location.replace("login.html");
+    } else {
+        showSignupError(result.message || "Signup failed");
+
+        document.getElementById("signupPassword").value = "";
+    }
 });
 
 
 // Switch
 function showSignup() {
     document.getElementById("container").classList.add("signup");
+
+    document.getElementById("loginForm").reset();
+
+    document.getElementById("loginError").innerText = "";
 }
 
 function showLogin() {
     document.getElementById("container").classList.remove("signup");
+
+    document.getElementById("signupForm").reset();
+
+    document.getElementById("signupError").innerText = "";
 }
 
 window.showSignup = showSignup;
@@ -86,4 +122,14 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+
+// Error 
+function showLoginError(msg) {
+    document.getElementById("loginError").innerText = msg;
+}
+
+function showSignupError(msg) {
+    document.getElementById("signupError").innerText = msg;
+}
 
